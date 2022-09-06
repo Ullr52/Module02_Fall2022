40b933dd95895423b8807fe034d6fe0c97ecf3b7
Ullr52 <97651235+Ullr52@users.noreply.github.com>
2022-09-05 22:38:06 -0500

    String Assignment

---
+++ Topic 1 Strings and Lists/String Assignment.py
@@ -0,0 +1,12 @@
+x = 'Alan Grant in Jurassic Park'
+print(x.capitalize())
+print(x.find('J'))
+print(x.index('in'))
+print(x.isalnum())
+print(x.isalpha())
+print(x.isdigit())
+print(x.islower())
+print(x.isupper())
+print(x.isspace())
+print(x.startswith('Alan'))
+
