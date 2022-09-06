2b68bc8f8ded7b4babdd890799047f35aea48951
Ullr52 <97651235+Ullr52@users.noreply.github.com>
2022-09-06 00:23:07 -0500

    Average Scores

---
+++ format_output/average_scores.py
@@ -0,0 +1,11 @@
+last_name = input('Enter your last name: ')
+first_name = input('Enter your first name: ')
+age = int(input('Enter your age: '))
+score1 = float(input('Enter your first score: '))
+score2 = float(input('Enter your second score: '))
+score3 = float(input('Enter your third score: '))
+
+average = (score1 + score2 + score3)/3
+
+print('{}, {} Age: {}, average grade: {:.2f}'.format(last_name, first_name,age,average))
+
